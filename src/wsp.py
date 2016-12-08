68c58ec72926cbb179b365bed347b15807efd762
rambius <rambiusparkisanius@gmail.com>
2016-12-08 08:08:35 +0200

    added logging

--- src/wsp.py
+++ src/wsp.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 
 import httplib
+import logging
 import re
 import sys
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 class ServerProbeResult:
 
@@ -14,18 +19,22 @@ class ServerProbeResult:
 def parse_server_header(server_hdr):
   m = re.match(r"([\w-]*)/([\w\.]*)", server_hdr)
   if m:
+    logger.info("Found match in header '%s'" % server_hdr)
     return (m.group(1), m.group(2))
   return None 
 
 def probe_server(server):
   conn = httplib.HTTPConnection(server)
-  conn.request("HEAD", "/")
+  method = "HEAD"
+  logger.info("Sending %s request to %s" % (method, server))
+  conn.request(method, "/")
   resp = conn.getresponse()
   spr = ServerProbeResult()
-  status = resp.status
-  if status == httplib.FORBIDDEN:
+  logger.info("Status code for %s request is %d" % (method, resp.status))
+  if resp.status == httplib.FORBIDDEN:
     spr.listing = False
   server_hdr = resp.getheader('server')
+  logger.debug("Found server header '%s'" % server_hdr)
   if server_hdr:
     s = parse_server_header(server_hdr)
     spr.name = s[0]
@@ -34,6 +43,7 @@ def probe_server(server):
 
 def main():
   for server in sys.argv[1:]:
+    logger.debug("Probing %s" % server)
     spr = probe_server(server)
     print spr.name, spr.version, spr.listing
 
