1f18a5465586791dacd23dbf2fbb9c7a2b02617c
rambius <rambiusparkisanius@gmail.com>
2016-12-09 08:27:10 +0200

    added enum for dir listing

--- src/wsp.py
+++ src/wsp.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python3.5
 
+import enum
 import http
 import http.client
 import logging
 import logging.config
+
 import re
 import sys
 
 logging.config.fileConfig("wsp-logging.conf")
 logger = logging.getLogger(__name__)
 
+class DirListing(enum.Enum):
+  unknown = 0
+  disabled = 1
+  enabled = 2
+
+  def __str__(self):
+    return self.name
+
 class ServerProbeResult:
 
-  def __init__(self, name=None, version=None, listing=True):
+  def __init__(self, name=None, version=None, listing=DirListing.enabled):
     self.name = name
     self.version = version
     self.listing = listing
@@ -25,6 +35,15 @@ def parse_server_header(server_hdr):
     logger.warn("Could not match header '%s'" % server_hdr)
   return None
 
+def get_dir_listing(status):
+  if status == http.HTTPStatus.FORBIDDEN:
+    dl = DirListing.disabled
+  elif status == http.HTTPStatus.UNAUTHORIZED:
+    dl = DirListing.unknown
+  else:
+    dl = DirListing.enabled
+  return dl
+
 def send_request(server, method="HEAD", uri="/"):
   conn = http.client.HTTPConnection(server)
   logger.info("Sending %s request to %s" % (method, server))
@@ -36,8 +55,9 @@ def probe_server(server):
   resp = send_request(server) 
   spr = ServerProbeResult()
   logger.info("Status code for probing request is %d" % resp.status)
-  if resp.status == http.HTTPStatus.FORBIDDEN:
-    spr.listing = False
+  dl = get_dir_listing(resp.status)
+  spr.listing = dl
+  logger.debug("Directory listing is %s" % spr.listing)
   server_hdr = resp.getheader('server')
   logger.debug("Found server header '%s'" % server_hdr)
   if server_hdr:
